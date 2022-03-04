4ff6ba07c68cfe597e24493dcc939cb55d08ac02
Blessing Wisdom <prodigeebw@gmail>
2022-03-04 08:40:09 +0100

    still ui

--- lib/movies/ui/movie_home.dart
+++ lib/movies/ui/movie_home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:movies/movies/bloc/upcoming_movies_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,15 +13,37 @@ class MovieHome extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 2, 86, 131),
         body: BlocBuilder<UpcomingMovieBloc, UpcomingMovieState>(
           builder: ((context, state) => state.when(
-              loading: () => const CircularProgressIndicator(),
+              loading: () => Center(
+                    child: CircularProgressIndicator(),
+                  ),
               loaded: (movieResponse) {
                 return ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    padding: const EdgeInsets.all(20.0),
-                    shrinkWrap: true,
-                    itemBuilder: (_, index) => Text(
-                          '${movieResponse.movies[index]}',
-                        ));
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.all(20.0),
+                  shrinkWrap: true,
+                  itemBuilder: (_, index) => Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 233, 196, 208),
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              height: 240.0,
+                              width: 250.0,
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
               })),
         ));
   }
